8b19e558dccad99312039c801c99e700a40b4155
KRom-i <emailroma@mail.ru>
2022-01-15 21:37:35 +0300

    builder-commit

---
+++ home-work-1/src/main/java/builder/Person.java
@@ -0,0 +1,119 @@
+package builder;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String country;
+    private String address;
+    private String phone;
+    private Integer age;
+    private String gender;
+
+    private Person () {
+    }
+
+    public String getFirstName () {
+        return firstName;
+    }
+
+    public String getLastName () {
+        return lastName;
+    }
+
+    public String getMiddleName () {
+        return middleName;
+    }
+
+    public String getCountry () {
+        return country;
+    }
+
+    public String getAddress () {
+        return address;
+    }
+
+    public String getPhone () {
+        return phone;
+    }
+
+    public Integer getEge () {
+        return age;
+    }
+
+    public String getGender () {
+        return gender;
+    }
+
+    public static Builder createBuilder(){
+        return new Builder ();
+    }
+
+    @Override
+    public String toString () {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", country='" + country + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+
+    public static class Builder{
+
+        private final Person person;
+
+        private Builder () {
+            this.person = new Person ();
+        }
+
+        public Builder withFirstName(String firstName){
+            this.person.firstName = firstName;
+            return this;
+        }
+
+        public Builder withLastName(String lastName){
+            this.person.lastName = lastName;
+            return this;
+        }
+
+        public Builder withMiddleName(String middleName){
+            this.person.middleName = middleName;
+            return this;
+        }
+
+        public Builder withCountry(String country){
+            this.person.country = country;
+            return this;
+        }
+
+        public Builder withAddress(String address){
+            this.person.address = address;
+            return this;
+        }
+
+        public Builder withPhone(String phone){
+            this.person.phone = phone;
+            return this;
+        }
+
+        public Builder withAge(Integer age){
+            this.person.age = age;
+            return this;
+        }
+
+        public Builder withGender(String gender){
+            this.person.gender = gender;
+            return this;
+        }
+
+        public Person build(){
+            return this.person;
+        }
+    }
+}
